8afc78565cbb23fbdfbad9ebf79ced1d96837594
Kush Daga <kushdaga@hotmail.com>
2020-10-16 18:53:16 +0530

    Added commnets

--- README.md
+++ README.md
@@ -4,7 +4,16 @@ A wheel of fortune spinner game that can be installed as a PWA on your phones. U
 
 > PWA App made with React! All the results being stored in a google sheet - [URL](https://docs.google.com/spreadsheets/d/1f_kNiKPon6y7vmHG3j1BaebXnRjHLeszGASUpCjl01I/edit?usp=sharing)
 
-## Screenshots
+# Details for Installation
+
+> Make sure you have Node.js installed in your computer.
+
+- Fork and Clone this repository
+- Run `npm install` or `yarn` in the base directory.
+- Run `npm start` or `yarn start` to run the app.
+- Make sure to rename `.env.example` to `.env` and add in your credentials
+
+# Screenshots
 
 ### Spreadsheet data
 
--- src/components/Wheel/index.jsx
+++ src/components/Wheel/index.jsx
@@ -34,44 +34,44 @@ export default function Wheel() {
     {
       text: "Double the Savings",
     },
-  ];
-  const [currentAngle, setCurrentAngle] = useState(0);
+  ]; // Hardcoded list of prizes
+  const [currentAngle, setCurrentAngle] = useState(0); //The angle that its currently rotated at when rotated by gesture
   const spinnerRef = useRef(null);
   const interactionRef = useRef(null);
-  const [text, setText] = useState("Spin");
+  const [text, setText] = useState("Spin"); //Text to display in button
   useEffect(() => {
     console.log(rotate);
-    const normalizedAngle = parseInt(rotate) + 30;
-    const baseAngle = normalizedAngle % 360;
-    const index = Math.floor(baseAngle / 45);
+    const normalizedAngle = parseInt(rotate) + 30; // Adding 30 deg to account for initial rotation in svg
+    const baseAngle = normalizedAngle % 360; //Getting the factor of rotation
+    const index = Math.floor(baseAngle / 45); //Getting the index ie in which part has the result landed
     // console.log(index, prizes[index]);
     if (rotate > 0) {
       setTimeout(() => {
-        alert("You won - " + prizes[index].text);
-        addScoreToGSheets(index);
+        alert("You won - " + prizes[index].text); //To display the result
+        addScoreToGSheets(index); //Adding result to google sheet
       }, 3000);
     }
   }, [rotate]);
 
   useEffect(() => {
     if (spinnerRef.current && interactionRef.current) {
-      var flag = false;
-      var region = new ZingTouch.Region(interactionRef.current);
-      var tempCurrentAngle = 0;
+      var flag = false; //To make sure the setTimeout isnt called multiple times
+      var region = new ZingTouch.Region(interactionRef.current); //Calling Zingtouch library
+      var tempCurrentAngle = 0; //Temp current angle to acount for rotation
       region.bind(interactionRef.current, "rotate", function (e) {
-        tempCurrentAngle += e.detail.distanceFromLast;
+        tempCurrentAngle += e.detail.distanceFromLast; //Adding rotation
         setCurrentAngle(tempCurrentAngle);
         // console.log(tempCurrentAngle, currentAngle);
         if (tempCurrentAngle < 0) {
           spinnerRef.current.style.transform =
-            "rotate(" + tempCurrentAngle + "deg)";
+            "rotate(" + tempCurrentAngle + "deg)"; //Actually showing the rotation.
           if (tempCurrentAngle < -360 && !flag) {
+            //Once it crosses the threshold.
             flag = true;
             setText("Will spin automatically in 3s");
             setTimeout(() => {
-              region.unbind(interactionRef.current, "rotate");
-              animateSpinner(tempCurrentAngle);
-
+              region.unbind(interactionRef.current, "rotate"); //Unbind the interaction event
+              animateSpinner(tempCurrentAngle); //Animate the spinner automatically after 3 secs
               console.log("unbinded", tempCurrentAngle);
             }, 3000);
           }
@@ -81,7 +81,7 @@ export default function Wheel() {
   }, [spinnerRef, interactionRef]);
 
   const animateSpinner = (tempCurrentAngle) => {
-    const randomAngle = Math.floor(Math.random() * (360 - 300 + 1)) + 300;
+    const randomAngle = Math.floor(Math.random() * (360 - 300 + 1)) + 300; // Get a random angle between 300 and 360 - Can be changed
     setRotate((rotate) => {
       console.log("In here", randomAngle, currentAngle, rotate);
       if (currentAngle === 0 && typeof tempCurrentAngle === "number") {
@@ -109,6 +109,11 @@ export default function Wheel() {
       </motion.object>
       <ProgressContainer>
         <ProgressBar>
+          {/*Added the width of the color in progress bar, with a max of 1500 as rotation
+              so % of color  =  (progress/1500) * 100
+              Also progress = currentAngle * -1
+              Have the threshold at 350 i.e. around 23%
+          */}
           <div style={{ width: `${(currentAngle * -1) / 15}%` }}></div>
         </ProgressBar>
         <Threshold></Threshold>
