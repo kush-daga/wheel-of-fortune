3d21adc79bee4fe136787119a3ad520562a0e1a8
Kush Daga <kushdaga@hotmail.com>
2020-10-16 19:05:25 +0530

    debugging

--- src/utils/addScoreToGSheets.js
+++ src/utils/addScoreToGSheets.js
@@ -2,12 +2,15 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 
 export const addScoreToGSheets = async (index) => {
   const doc = new GoogleSpreadsheet(process.env.REACT_APP_SPREADSHEET_ID);
+  console.log(doc);
   await doc.useServiceAccountAuth({
     client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL,
     private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
   });
+  console.log(doc);
   await doc.loadInfo();
   const sheet = doc.sheetsByIndex[0];
+
   await sheet.addRow({
     web_client: "kushdaga pwa",
     timestamp: Date.now(),
