00376e17dc40d5ba7a2e6fcf8233fb1771edb271
Kush Daga <kushdaga@hotmail.com>
2020-10-16 19:38:25 +0530

    Debugging env vars

--- src/utils/addScoreToGSheets.js
+++ src/utils/addScoreToGSheets.js
@@ -1,30 +1,37 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
 export const addScoreToGSheets = async (index) => {
-  try {
-    const doc = new GoogleSpreadsheet(process.env.REACT_APP_SPREADSHEET_ID);
-    await doc.useServiceAccountAuth(
-      {
-        client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
-      },
-      (err) => {
-        if (err) console.log(err);
-        console.log("Working");
-      }
-    );
-    await doc.loadInfo();
-    const sheet = doc.sheetsByIndex[0];
-    console.log("Sheedsdt", sheet);
-    await sheet.addRow({
-      web_client: "kushdaga pwa",
-      timestamp: Date.now(),
-      spin_result_index: index,
-    });
-    window.location.reload();
-  } catch (err) {
-    console.log("Errror", err);
-    // window.location.reload();
+  if (
+    process.env.REACT_APP_GOOGLE_CLIENT_EMAIL &&
+    process.env.REACT_APP_GOOGLE_PRIVATE_KEY
+  ) {
+    try {
+      const doc = new GoogleSpreadsheet(process.env.REACT_APP_SPREADSHEET_ID);
+      await doc.useServiceAccountAuth(
+        {
+          client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL,
+          private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
+        },
+        (err) => {
+          if (err) console.log(err);
+          console.log("Working");
+        }
+      );
+      await doc.loadInfo();
+      const sheet = doc.sheetsByIndex[0];
+      console.log("Sheedsdt", sheet);
+      await sheet.addRow({
+        web_client: "kushdaga pwa",
+        timestamp: Date.now(),
+        spin_result_index: index,
+      });
+      window.location.reload();
+    } catch (err) {
+      console.log("Errror", err);
+      // window.location.reload();
+    }
+  } else {
+    alert("Env variables not set");
   }
 
   //   console.log(sheet);
