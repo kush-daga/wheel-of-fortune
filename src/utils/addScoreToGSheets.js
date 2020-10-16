1b0bb530e37047483884bb8525e1792cc5f8bcba
Kush Daga <kushdaga@hotmail.com>
2020-10-16 19:27:21 +0530

    error handling

--- src/utils/addScoreToGSheets.js
+++ src/utils/addScoreToGSheets.js
@@ -1,26 +1,31 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
 export const addScoreToGSheets = async (index) => {
-  const doc = new GoogleSpreadsheet(process.env.REACT_APP_SPREADSHEET_ID);
-  await doc.useServiceAccountAuth(
-    {
-      client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
-    },
-    (err) => {
-      if (err) console.log(err);
-      console.log("Working");
-    }
-  );
-  console.log(" yo", doc);
-  await doc.loadInfo();
-  const sheet = doc.sheetsByIndex[0];
-  console.log("Sheedsdt", sheet);
-  await sheet.addRow({
-    web_client: "kushdaga pwa",
-    timestamp: Date.now(),
-    spin_result_index: index,
-  });
-  window.location.reload();
+  try {
+    const doc = new GoogleSpreadsheet(process.env.REACT_APP_SPREADSHEET_ID);
+    await doc.useServiceAccountAuth(
+      {
+        client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
+      },
+      (err) => {
+        if (err) console.log(err);
+        console.log("Working");
+      }
+    );
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    console.log("Sheedsdt", sheet);
+    await sheet.addRow({
+      web_client: "kushdaga pwa",
+      timestamp: Date.now(),
+      spin_result_index: index,
+    });
+    window.location.reload();
+  } catch (err) {
+    console.log("Errror", err);
+    window.location.reload();
+  }
+
   //   console.log(sheet);
 };
